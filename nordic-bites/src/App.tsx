f39852a984c55ae70f6a21640ee2adf3e5f7549d
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2025-12-02 10:20:30 +0100

    added routing

--- nordic-bites/src/App.tsx
+++ nordic-bites/src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AboutPage from "./pages/About/AboutPage";
-import Home from "./pages/Home/Home";
+//import Home from "./pages/Home/Home";
+//mport CheckoutPage from "./pages/Checkout/CheckoutPage";
+//import ConfirmationPage from "./pages/Confirmation/ConfirmationPage";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/about" element={<AboutPage />} />
+        {/* <Route path="/cart" element={<CartPage />} /> */}
+        {/* <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/confirmation" element={<ConfirmationPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
